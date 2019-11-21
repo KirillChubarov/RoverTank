0dc87ac9f95c3a9ce23a091c881648e34a858df3
Kirill Chubarov <chubakka66@gmail.com>
2019-11-21 18:32:23 +0300

    fix update script

--- www/html/scripts/update_from_github.sh
+++ www/html/scripts/update_from_github.sh
@@ -6,6 +6,6 @@ git pull
 cd /root/git/RoverTank/motor_ctrl
 make
 cd /root/git/RoverTank
-rsync -av --delete /root/git/RoverTank/www /var/www
+rsync -av --delete /root/git/RoverTank/www /var
 find /var/www/html/scripts -type f | xargs chmod 4755
 echo 'Update complete'
