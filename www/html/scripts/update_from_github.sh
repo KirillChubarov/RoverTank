0d4ea4f1496b683bc326c360ec4f3c880f46620e
Kirill Chubarov <chubakka66@gmail.com>
2019-11-21 18:53:30 +0300

    Update update_from_github.sh

--- www/html/scripts/update_from_github.sh
+++ www/html/scripts/update_from_github.sh
@@ -8,6 +8,6 @@ make
 cd /root/git/RoverTank
 rm -r /var/www/
 mkdir /var/www/
-cp /root/git/RoverTank/www /var/www
+cp -avr /root/git/RoverTank/www /var/www
 find /var/www/html/scripts -type f | xargs chmod 4755
 echo 'Update complete'
